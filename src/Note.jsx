80615b06e9acd3a6e06e45984cfa8556dd33e644
hamsa-09 <hamsavardhinibaskar@gmail.com>
2024-11-11 14:59:41 +0530

    3rd

--- src/Note.jsx
+++ src/Note.jsx
@@ -6,13 +6,26 @@ import{Link } from 'react-router-dom'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function Note() {
-  const [todo, setTodo] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [search,setSearch]=useState("");
+  const[filterTask,setFilterTask]=useState([]);
 
   const value = collection(db, 'tasks');
   const getData = async () => {
     const dbVal = await getDocs(value);
-    setTodo(dbVal.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    setTodos(dbVal.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
+
+  useEffect(()=>{
+    if(search.trim()===""){
+      setFilterTask(todos);
+    }
+    else{
+      const filtered=todos.filter(todo=> todo.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilterTask(filtered);
+    }
+  },[search,todos]);
 
   useEffect(() => {
     getData();
@@ -21,19 +34,27 @@ export default function Note() {
   const handleDelete =async(id)=>{
     const delval=doc(db,'tasks',id);
     await deleteDoc(delval);
-    setTodo(todo.filter((item)=>item.id!==id))
+    setTodos(todos.filter((item)=>item.id!==id))
   }
 
   return (
     <div style={{position:"relative"}}>
-      <div className='header'><h1>Todo-App</h1>
+      <div className='header'><h1>Todos-App</h1>
       <Link to="/create" className="icon-link">
       <i className="fa-solid fa-circle-plus"></i>
         </Link></div>
- 
-        <div className="List">
-        {todo.length > 0 ? (
-          todo.map((item) => (
+        <div className="search-input">
+        <input 
+        type="text" 
+        placeholder="Search..." 
+        value={search} 
+        onChange={(e) => setSearch(e.target.value)} 
+      />
+        </div>
+       
+      <div className="List">
+        {todos.length > 0 ? (
+          filterTask.map((item) => (
             <div key={item.id} className="Notes">
               <h2>{item.title}</h2>
               <p>{item.note}</p>
